c33877aa5fa8bc6e36a92595e2dff301557731b6
Jatin-Patel-Glue <jat.patel@reply.com>
2025-01-03 15:13:54 +0000

    Header and sidebar changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,14 +8,12 @@ const Header = () => {
         console.log("E")
     }
     return (
-        <div className='header'>
-            <button className='glue-logo' onClick={handleHomeClick}>
+        <div class="header shadow-lg">
+            <button class='glue-logo' onClick={handleHomeClick}>
                 <img src={GlueLogo} />
             </button>
-            <button onClick={handleHomeClick}><h1 className = 'home'>Glue Log Forwarder</h1></button>
-            <button className='refresh'>
-                <img src = {RefreshIcon} />
-            </button>
+            <button onClick={handleHomeClick}><h1 class = "home-button">Glue Log Forwarder</h1></button>
+            <button class="w-10 h-10 bg-white"><img src = {RefreshIcon} /></button>
         </div>
     )
 };
--- src/components/Sections.js
+++ src/components/Sections.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({children, height, width}) => {
     return (
         <div 
-            className={`bg-sidebarBg rounded-lg shadow-lg p-6 mb-4 ${height} ${width}`}>
+            className={`bg-sidebarBg rounded-lg shadow-lg p-6 ${height} ${width}`}>
                 {children}
         </div>
     )
